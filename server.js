131afcbd40c7cfd004759e865539e7ce8e73a861
nattyait <nattanicha@gmail.com>
2018-02-10 11:16:29 +0700

    Get post by id :baby:

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/posts', async (req, res) => {
   res.json(posts)
 })
 
+app.get('/posts/:id', async (req, res) => {
+  const post = await Post.get(req.params.id)
+  if (!post) {
+    return res.sendStatus(404)
+  }
+  res.json(post)
+})
+
 app.listen(3000, () => {
   console.log('started')
 })
